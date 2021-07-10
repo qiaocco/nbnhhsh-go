096d59c4c93dfa393eb1ce946a9fd5718a99f7e0
qiaocc <qiaocco@gmail.com>
2021-07-10 22:55:22 +0800

    生成命令行模板

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"nbnhhsh-go/lib"
 	"os"
 )
 
@@ -12,16 +13,12 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "hhsh",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Args: cobra.ExactArgs(1),
+	Short: "拼音首字母缩写翻译工具",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("关键词是:" + args[0])
+		query := lib.NewQuery(args[0])
+		wordList := query.DoQuery()
+		lib.PrintTable(wordList)
 	},
 }
 
